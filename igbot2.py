82ec3697da2b57d8f017046d15409f1d0e49bb93
Erika Redding <overdoseone@Erikas-MacBook-Air.local>
2021-02-03 23:32:15 -0500

    updated py and added ignore

--- igbot2.py
+++ igbot2.py
@@ -1,8 +1,22 @@
 import scheduler
-from instapy import Instapy
+from instapy import InstaPy
+import dotenv
+import os
+
+# load credentials
+dotenv.load_dotenv('.env')
+
+# grab credentials for us
+un = os.environ.get('CONSUMER_KEY')
+pwd = os.environ.get('CONSUMER_SECRET')
 
 
-def runtime():
-    pass
+ht = ['mercuryretrograde']
+users = ['nish_rdh', 'pradadagod']
+session = InstaPy(username=un, password=pwd)
+session.login()
+session.like_by_tags(ht, amount=2, skip_top_posts=False)
+# session.set_do_story(enabled = True, percentage = 70, simulate = False)
+# session.story_by_users(users)
 
 # git config –global core.editor "C:\Program Files (x86)\Notepad++\notepad++.exe"
